850fe506247c575556e5f5202054eb86f6dec333
Joaquin Merida <joaquinmerida1997@gmail.com>
2023-07-19 22:23:58 -0300

    verpedidos

---
+++ src/components/Comanda.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./styles/Comanda.css"
+
+
+const Comanda = ({pedido}) => {
+return (
+    <table className='comandaTable'>
+        <tr>
+        <th className='headerTable' colSpan={3}>Mesa seleccionada: {pedido.mesa}</th>
+        <th className='headerTable'> {pedido.horapedido}</th>
+        </tr>
+        <tr>
+            <th>N°</th>
+            <th>Plato</th>
+            <th>Estado</th>
+            <th>$</th>
+        </tr>
+        {pedido.items.map((item, index) =>(
+            <tr key={index}>
+            <td>{index+1}</td>
+            <td>{item.plato}</td>
+            <td>{item.estado}</td>
+            <td>{item.precio}</td>
+            </tr>
+        ))}
+    </table>
+)
+}
+
+export default Comanda
--- src/pages/VerPedidos.jsx
+++ src/pages/VerPedidos.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Comanda from '../components/Comanda'
 
 const VerPedidos = () => {
+const pedido = {
+    mesa: '1',
+    horapedido: '15:00',
+    items: [
+        { plato: 'Nombre plato 1', estado: 'Pendiente', precio: 10.99 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        // Más items del pedido...
+    ],
+};
+
+const pedido2 = {
+    mesa: '3',
+    horapedido: '12:00',
+    items: [
+        { plato: 'Nombre plato 1', estado: 'Pendiente', precio: 10.99 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+        { plato: 'Nombre plato 2', estado: 'En preparación', precio: 12.50 },
+         // Más items del pedido...
+    ],
+};
+
+if (!pedido) {
+    return <div>Cargando pedido...</div>; // O algún mensaje de error o estado de carga
+}
+
     return (
-        <div>VerPedidos</div>
+        <div className='comandasWrapper'>
+            <Comanda pedido={pedido}/>
+            <Comanda pedido={pedido2}/>
+        </div>
     )
 }
 
